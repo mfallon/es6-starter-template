0d12f011024f068022b90e14eb994891f71d76a4
Mark Fallon <mark.fallon@wipro.com>
2017-03-13 21:32:37 +0000

    adding basics

---
+++ webpack.dev.js
@@ -0,0 +1,110 @@
+"use strict";
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const SassLintPlugin = require('sasslint-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const version = require('./package.json').version;
+const scriptLoader = require('script-loader');
+
+module.exports = {
+  entry: [
+    './src/js/main.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '/js/bundle.[hash].js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    port: 3001,
+    historyApiFallback: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new ExtractTextPlugin('/css/main.[hash].css'),
+    new webpack.DefinePlugin({
+      'process.env.TEST_API': JSON.stringify(process.env.TEST_API),
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'process.env.VERSION': JSON.stringify(version)
+    }),
+    new SassLintPlugin({
+      glob: 'src/scss/**/*.s?(a|c)ss',
+      ignorePlugins: ['extract-text-webpack-plugin']
+    })
+  ],
+  resolve: {
+    modulesDirectories: [path.join(__dirname, 'src/js/'), 'node_modules'],
+    extensions: ['', '.js', '.jsx', '.json'],
+    alias: {
+      actions: path.join(__dirname, 'src/js/actions'),
+      api: path.join(__dirname, 'src/js/api'),
+      components: path.join(__dirname, 'src/js/components'),
+      containers: path.join(__dirname, 'src/js/containers'),
+      helpers: path.join(__dirname, 'src/js/helpers'),
+      middleware: path.join(__dirname, 'src/js/middleware'),
+      reducers: path.join(__dirname, 'src/js/reducers'),
+      stores: path.join(__dirname, 'src/js/stores'),
+      scss: path.join(__dirname, 'src/scss')
+    }
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loaders: ['babel-loader', 'eslint'],
+        exclude: /node_modules/
+      }, {
+        test: /\.html$/,
+        loader: 'htmlhint',
+        exclude: /node_modules/
+      }
+    ],
+    loaders: [{
+      test: /\.(js)/,
+      exclude: /node_modules/,
+      // cacheable: true,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react'],
+        plugins: ['transform-runtime']
+      }
+    },
+    {
+      test: /\.(jsx)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    },
+    {
+      test: /.json/,
+      exclude: [/node_modules/, /config/],
+      loader: 'json-loader'
+    }, {
+      test: /\.html$/,
+      exclude: /node_modules/,
+      loader: 'html'
+    }, {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract('css!sass')
+    },
+    {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    },
+    {
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: 'url-loader?name=fonts/[name].[ext]&minetype=application/font-woff'
+    },
+    {
+      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: 'url-loader?name=fonts/[name].[ext]'
+    }]
+  }
+};
